7b11c8b325903ad983105568ee19e5e9faf44f63
Saurav Singh <sauravcloud26@gmail.com>
2026-02-18 18:58:09 +0530

    tax-calculator complete

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,12 @@
  */
 export function calculateTax(income) {
   // Your code here
+  if (income <= 10000) return 0;
+  if (income <= 30000) {
+    return 0.1 * (income - 10000);
+  }
+  if (income <= 70000) {
+    return 2000 + 0.2 * (income - 30000);
+  }
+  return 10000 + 0.3 * (income - 70000);
 }
